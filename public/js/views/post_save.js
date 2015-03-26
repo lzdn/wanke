39921dabf6f375e005034601af48b9310f27f4f0
sunshuang320 <825347993@qq.com>
2015-03-26 10:37:46 +0800

    修改post_save页面体验

--- public/js/views/post_save.js
+++ public/js/views/post_save.js
@@ -2,28 +2,37 @@
  * Created by amberglasses on 15/3/24.
  */
     $(function(){
+        //var aNav=document.getElementsByClassName("am-btn-idx");
+        //  for(var i=0;i<aNav.length;i++){
+        //         aNav[i].onclick=function(){
+        //            for(var j=0;j<aNav.length;j++){
+        //                aNav[j].className="am-btn-idx am-btn";
+        //            }
+        //             this.className="am-btn-idx am-btn selectbtn";
+        //         }
+        //     }
+
         var aNav=document.getElementsByClassName("am-btn-extend");
-           //console.log(aNav.length);
-          for(var i=0;i<aNav.length;i++){
-                 aNav[i].onclick=function(){
-                     console.log(aNav.length);
-                    for(var j=0;j<aNav.length;j++){
-                        aNav[j].className="am-btn-extend am-btn am-btn-link";
-                    }
-                     this.className="am-btn-extend am-btn am-btn-primary am-round"
-                 }
-             }
-        var userValues = $("#doc-ta-1").val();
+        for(var i=0;i<aNav.length;i++){
+            aNav[i].onclick=function(){
+                 fontLength(i);
+        }
+            function fontLength(idx){
+                for(var j=0;j<aNav.length;j++){
+                    aNav[j].className="am-btn-extend am-btn am-btn-link am-round";
+                }
+                aNav[idx].className="am-btn-extend am-btn am-btn-primary am-round";
+            }
+            }
+        }
+
         $("#doc-ta-1").keydown(function(){
               var aUserval =$("#doc-ta-1").val();
-              var oMaxleg=0;
-            if(aUserval.length>140){
-                oMaxleg=140-aUserval.length;
-                $("#usr-say-leg").html("<p>"+oMaxleg+"</p>");
-                console.log(aUserval.length);
+            //alert(aUserval.length);
+           if(aUserval.length>140){
+                $(".usr-say-leg-2").html("<p>"+aUserval.length+"</p>").addClass("maxlegcss");
             }else{
-                $("#usr-say-leg").html("<p>"+aUserval.length+"/140</p>");
-                console.log(aUserval.length);
+                $(".usr-say-leg-2").html("<p>"+aUserval.length+"</p>").removeClass("maxlegcss");
             }
             if($("#doc-ta-1").val()!=""){
                 $("#usr-sbm").removeClass("am-disabled");
@@ -31,22 +40,26 @@
                 $("#usr-sbm").addClass("am-disabled");
             }
         });
-
         $("#usr-sbm").on("click",function(){
-            $("#modal-confirm").modal({
-                //relatedTarget: this,
-                onConfirm: function() {
-                    alert('aa');
-                },
-                onCancel: function() {
-                    alert('bb');
-                }
-            });
+            var aUserval2=$("#doc-ta-1").val();
+            if(aUserval2.length>140){
+                //alert("haha");
+                $("#my-alert").modal();
+            }else{
+                $("#modal-confirm").modal({
+                    //relatedTarget: this,
+                    onConfirm: function() {
+                        alert('准备上传');
+                    },
+                    onCancel: function() {
+                    }
+                });
+            }
         });
 
-        $("").get(url,{},function(data){
-
-    },json)
+    //    $("").get(url,{},function(data){
+    //
+    //},json)
 
  });
 
