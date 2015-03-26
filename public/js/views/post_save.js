b2e0f546f47912ae7e3174175870acc7a1b9143b
sunshuang320 <825347993@qq.com>
2015-03-26 11:52:48 +0800

    修复post—save的文字显示bug问题

--- public/js/views/post_save.js
+++ public/js/views/post_save.js
@@ -2,43 +2,29 @@
  * Created by amberglasses on 15/3/24.
  */
     $(function(){
-        //var aNav=document.getElementsByClassName("am-btn-idx");
-        //  for(var i=0;i<aNav.length;i++){
-        //         aNav[i].onclick=function(){
-        //            for(var j=0;j<aNav.length;j++){
-        //                aNav[j].className="am-btn-idx am-btn";
-        //            }
-        //             this.className="am-btn-idx am-btn selectbtn";
-        //         }
-        //     }
-
         var aNav=document.getElementsByClassName("am-btn-extend");
         for(var i=0;i<aNav.length;i++){
             aNav[i].onclick=function(){
-                 fontLength(i);
-        }
-            function fontLength(idx){
                 for(var j=0;j<aNav.length;j++){
                     aNav[j].className="am-btn-extend am-btn am-btn-link am-round";
                 }
-                aNav[idx].className="am-btn-extend am-btn am-btn-primary am-round";
-            }
+                this.className="am-btn-extend am-btn am-btn-primary am-round";
             }
         }
-
         $("#doc-ta-1").keydown(function(){
-              var aUserval =$("#doc-ta-1").val();
-            //alert(aUserval.length);
-           if(aUserval.length>140){
-                $(".usr-say-leg-2").html("<p>"+aUserval.length+"</p>").addClass("maxlegcss");
-            }else{
-                $(".usr-say-leg-2").html("<p>"+aUserval.length+"</p>").removeClass("maxlegcss");
-            }
-            if($("#doc-ta-1").val()!=""){
-                $("#usr-sbm").removeClass("am-disabled");
-            }else{
-                $("#usr-sbm").addClass("am-disabled");
-            }
+            setTimeout(function(){
+                var aUserval =$("#doc-ta-1").val();
+                if(aUserval.length>140){
+                    $(".usr-say-leg-2").html("<p>"+aUserval.length+"</p>").addClass("maxlegcss");
+                }else{
+                    $(".usr-say-leg-2").html("<p>"+aUserval.length+"</p>").removeClass("maxlegcss");
+                }
+                if($("#doc-ta-1").val()!=""){
+                    $("#usr-sbm").removeClass("am-disabled");
+                }else{
+                    $("#usr-sbm").addClass("am-disabled");
+                }
+            },100);
         });
         $("#usr-sbm").on("click",function(){
             var aUserval2=$("#doc-ta-1").val();
