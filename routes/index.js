b74699d39ecf361822b11cc4cd2b51abcdcdc681
AmberGlasses <775837740@qq.com>
2015-03-31 13:06:30 +0800

    实现JS-SDK配置文件的获取

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,5 @@
 var express = require('express');
 var router = express.Router();
-var request= require('request');
 var token = 'dianyingren';
 
 /* GET home page. */
--- routes/weixin.js
+++ routes/weixin.js
@@ -5,19 +5,9 @@ var express = require('express');
 var router = express.Router();
 var config = require('../config');
 var API = require('wechat-api');
-var api = new API(config.appId, config.appSecret, function (callback) {
-    // 传入一个获取全局token的方法
-    fs.readFile('access_token.txt', 'utf8', function (err, txt) {
-        if (err) {
-            return callback(err);
-        }
-        callback(null, JSON.parse(txt));
-    });
-}, function (token, callback) {
-    // 请将token存储到全局，跨进程、跨机器级别的全局，比如写到数据库、redis等
-    // 这样才能在cluster模式及多机情况下使用，以下为写入到文件的示例
-    fs.writeFile('access_token.txt', JSON.stringify(token), callback);
-});
+var fs = require('fs');
+
+var api = new API(config.appId, config.appSecret);
 
 /* GET users listing. */
 router.get('/', function (req, res, next) {
@@ -25,11 +15,13 @@ router.get('/', function (req, res, next) {
 });
 
 router.get('/getJsConfig', function (req, res) {
+    console.log(config);
     var param = {
         debug: false,
         jsApiList: ['chooseImage', 'previewImage'],
         url: config.authUrl + "/post_save.html"
     };
+    console.log(param);
     api.getJsConfig(param, function (err, result) {
         console.log(err);
         console.log('------------------------------');
