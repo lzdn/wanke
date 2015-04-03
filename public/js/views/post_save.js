30747f6a967afc7814176175dfe10c1ca411a4bd
sunshuang320 <825347993@qq.com>
2015-04-03 17:26:24 +0800

    测试24

--- public/js/views/post_save.js
+++ public/js/views/post_save.js
@@ -159,7 +159,7 @@ $(function () {
         $.get("http://123.57.14.126/weixin/getJsConfig", function (result) {
             alert(result);
             appId = result.appId+"";
-            jslist = result.jsApiList+"";
+            jslist = result.jsApiList;
             noncestr = result.nonceStr+"";
             signature = result.signature+"";
             timestamp = result.timestamp+"";
@@ -170,10 +170,10 @@ $(function () {
             console.log(timestamp);
             wx.config({
                 debug: true,// 开启调试模式,调用的所有api的返回值会在客户端alert出来，若要查看传入的参数，可以在pc端打开，参数信息会通过log打出，仅在pc端时才会打印。
-                appId: appId, // 必填，公众号的唯一标识
-                timestamp: timestamp, // 必填，生成签名的时间戳
-                nonceStr: noncestr, // 必填，生成签名的随机串
-                signature: signature,// 必填，签名，见附录1
+                appId: appId+"", // 必填，公众号的唯一标识
+                timestamp: timestamp+"", // 必填，生成签名的时间戳
+                nonceStr: noncestr+"", // 必填，生成签名的随机串
+                signature: signature+"",// 必填，签名，见附录1
                 jsApiList:[
                 'checkJsApi',
                 'chooseImage',
