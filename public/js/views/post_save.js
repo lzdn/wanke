968ec37a4ae65cf31413d09ecd175c1ffe9a4b08
sunshuang320 <825347993@qq.com>
2015-04-09 12:02:07 +0800

    接口测试23

--- public/js/views/post_save.js
+++ public/js/views/post_save.js
@@ -83,7 +83,7 @@ $(function () {
                             alert('已选择 ' +localIds.length + ' 张图片');
                             $("<img src=\""+localIds[0]+"\" alt=\"#\"/>").prependTo("#imgwall");
 
-                            $("<div class=\"imgnav imgnav-"+localIds[0]+ "\" ><a href=\"#\" value=\""+localIds[0]+"\ class=\"am-close\">&times;</a><img src=\""+localIds[0]+"\ alt=\"#\"/></div>").prependTo("#imgwall");
+
                                  $("#addimg").show();
                                  var aimgnav = $(".am-close");
                                  for (var i = 0; i < aimgnav.length; i++) {
