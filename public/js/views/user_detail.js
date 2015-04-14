c9c49966afe6506d9a4a3020f49eefd4b3830411
sunshuang320 <825347993@qq.com>
2015-04-14 16:55:42 +0800

    修改链接

--- public/js/views/user_detail.js
+++ public/js/views/user_detail.js
@@ -6,5 +6,10 @@
     $("#userpost").on("click",function(){
         window.location.href="user_post.html?";
     });
-
+    $("#user_address").on("click",function(){
+        window.location.href="user_address.html?";
+    });
+    $("#user_contact").on("click",function(){
+        window.location.href="user_contact.html?";
+    });
 })(jQuery);
