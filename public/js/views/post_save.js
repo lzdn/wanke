8fe56843d79875f719d0e68da66c9ca45975d292
sunshuang320 <825347993@qq.com>
2015-04-15 15:22:15 +0800

    文件测试4

--- public/js/views/post_save.js
+++ public/js/views/post_save.js
@@ -76,10 +76,13 @@ $(function () {
                     success: function (res) {
                         console.log(res);
                         alert(res);
-                        for(str in res)
-                        {
-                            alert(str);
-                        }
+                        //for(str in res)
+                        //{
+                        //    alert(str);
+                        //}
+                        alert(res.sourceType);
+                        alert(res.errMsg);
+
                         var file = new AV.File("myfile.txt", res);
                         file.save({
                             success:function(files){
