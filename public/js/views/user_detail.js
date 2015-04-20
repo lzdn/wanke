731fba15d84971ac00cdcc5e4cb02110d5c66fc6
sunshuang320 <825347993@qq.com>
2015-04-20 14:03:02 +0800

    用户测试23

--- public/js/views/user_detail.js
+++ public/js/views/user_detail.js
@@ -1,20 +1,49 @@
-
-(function($) {
-    var postview=window.location.search.split('=')[1];
-    var code=postview.split("&")[0];
+(function ($) {
+    var postview = window.location.search.split('=')[1];
+    var code = postview.split("&")[0];
     alert(code);
-    $.post("http://fuwuhao.dianyingren.com/weixin/userSignUp",{code:code},function(res){
+    $.post("http://fuwuhao.dianyingren.com/weixin/userSignUp", {code: code}, function (res) {
         alert(res);
-        console.log(res);
+        alert(res.authData);
+        alert(res.get("nickname"));
+        alert(res.get("openid"));
+        alert(res.get("headimgurl"));
     });
 
-    $("#userpost").on("click",function(){
-        window.location.href="user_post.html?";
+    $("#userpost").on("click", function () {
+        window.location.href = "user_post.html?";
     });
-    $("#user_address").on("click",function(){
-        window.location.href="user_address.html?";
+    $("#user_address").on("click", function () {
+        window.location.href = "user_address.html?";
     });
-    $("#user_contact").on("click",function(){
-        window.location.href="user_contact.html?";
+    $("#user_contact").on("click", function () {
+        window.location.href = "user_contact.html?";
     });
 })(jQuery);
+
+
+//{
+//    "weixin"
+//:
+//    {
+//        "sex"
+//    :
+//        1, "nickname"
+//    :
+//        "动名词", "city"
+//    :
+//        "Mudanjiang", "headimgurl"
+//    :
+//        "http://wx.qlogo.cn/mmopen/PiajxSqBRaEJgfrRe3VDiaNqFHsR4dBj8Z5rWgsr0icBXAiaY1DmjoNBg85PILc6WQw1sgACOUsGNibYp2QW5KgeRpw/0", "openid"
+//    :
+//        "omoCDjmkB3VOX-C8SX5-AfE6GmHU", "language"
+//    :
+//        "zh_CN", "province"
+//    :
+//        "Heilongjiang", "country"
+//    :
+//        "China", "privilege"
+//    :
+//        []
+//    }
+//}
