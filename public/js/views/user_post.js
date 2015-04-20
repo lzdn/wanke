5f7db8d68424e61a734b81b86ebb1d1ccbe0aa3a
sunshuang320 <825347993@qq.com>
2015-04-20 11:26:44 +0800

    更改4

--- public/js/views/user_post.js
+++ public/js/views/user_post.js
@@ -45,17 +45,17 @@
                 $("#users").on("click", function () {
                     window.location.href = "user_detail.html";
                 });
-                //$(".imgpreview").on("click",function(){
-                //    var cur= $(this).attr("src");
-                //    var  url=$(this).parent().attr("value");
-                //    var arr=url.split(",");
-                //    wx.previewImage({
-                //        current:cur, // 当前显示的图片链接
-                //        urls:arr// 需要预览的图片链接列表
-                //    });
-                //    event.stopPropagation();
-                //});
-                //$(".imgpreview").removeClass("imgpreview");
+                $(".imgpreview").on("click",function(){
+                    var cur= $(this).attr("src");
+                    var  url=$(this).parent().attr("value");
+                    var arr=url.split(",");
+                    wx.previewImage({
+                        current:cur, // 当前显示的图片链接
+                        urls:arr// 需要预览的图片链接列表
+                    });
+                    event.stopPropagation();
+                });
+                $(".imgpreview").removeClass("imgpreview");
 
             });
         }
