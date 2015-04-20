38a3445d824a9461e39622a999d1e1527452902b
sunshuang320 <825347993@qq.com>
2015-04-20 13:29:41 +0800

    用户测试21

--- public/js/views/user_detail.js
+++ public/js/views/user_detail.js
@@ -2,7 +2,8 @@
 (function($) {
     var postview=window.location.search.split('=')[1];
     var code=postview.split("&")[0];
-    $.post("http://fuwuhao.dianyingren.com/weixin/userSignUp?"+code+"",function(res){
+    alert(code);
+    $.post("http://fuwuhao.dianyingren.com/weixin/userSignUp",code,function(res){
         alert(res);
         console.log(res);
     });
