be166c9e2814e6c920ef04ce7667c74fa577e849
sunshuang320 <825347993@qq.com>
2015-04-20 11:25:52 +0800

    更改3

--- public/js/views/user_post.js
+++ public/js/views/user_post.js
@@ -3,7 +3,7 @@
     var userid ="55277430e4b0f543683906ac";
     var skx = -5;
     var postview
-
+    loadwx();
     $("#arrow").hide();
     loading(function () {
         $(".title").on("click", function () {
@@ -182,6 +182,26 @@
                     }
                 });
             }
+        });
+    }
+    function loadwx(){
+        var appId, jslist, noncestr, signature, timestamp, jsApiList;
+        $.get("http://fuwuhao.dianyingren.com/weixin/getJsConfig?page=post_index", function (result) {
+            console.log(result);
+            appId = result.appId;
+            jslist = result.jsApiList;
+            noncestr = result.nonceStr;
+            signature = result.signature;
+            timestamp = result.timestamp;
+            jsApiList = result.jsApiList;
+            wx.config({
+                debug: false,// 开启调试模式,调用的所有api的返回值会在客户端alert出来，若要查看传入的参数，可以在pc端打开，参数信息会通过log打出，仅在pc端时才会打印。
+                appId: appId, // 必填，公众号的唯一标识
+                timestamp: timestamp, // 必填，生成签名的时间戳
+                nonceStr: noncestr, // 必填，生成签名的随机串
+                signature: signature,// 必填，签名，见附录1
+                jsApiList: jsApiList// 必填，需要使用的JS接口列表，所有JS接口列表见附录2
+            });
         });
     }
 
