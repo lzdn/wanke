60120520efd96d2244e942079ba08b036d1b58b0
sunshuang320 <825347993@qq.com>
2015-04-20 18:52:41 +0800

    用户测试29

--- public/js/views/post_index.js
+++ public/js/views/post_index.js
@@ -105,6 +105,7 @@ $(".am-input-group-label").on("click",function(){
             if (currentUser) {
                 //alert("已登录")
                 alert(currentUser);
+                alert(currentUser.authData+currentUser.get("authData")+currentUser.weixin+currentUser.git("weixin"))
             } else {
                 alert("没有登录")
                 $.get("http://fuwuhao.dianyingren.com/weixin/getAuthUrl?page=user_detail",function(res){
