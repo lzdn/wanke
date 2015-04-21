5529792e366825c2c547085cf77417f0cb96299f
AmberGlasses <775837740@qq.com>
2015-04-21 21:45:32 +0800

    调整联系方式页面功能

--- public/js/views/user_detail.js
+++ public/js/views/user_detail.js
@@ -19,7 +19,7 @@
         AV.initialize("f7r02mj6nyjeocgqv7psbb31mxy2hdt22zp2mcyckpkz7ll8", "blq4yetdf0ygukc7fgfogp3npz33s2t2cjm8l5mns5gf9w3z");
         var code = "";
         var userlog, userid, queryobject, nickname;
-        var postview = window.location.search.split('?')[1];
+        var postview = window.location.search.split('?id=')[1];
         if (postview.indexOf("=") > 0) {
             userlog = window.location.search.split('=')[1];
             code = userlog.split("&")[0];
@@ -62,11 +62,13 @@
                     var $tpl = $('#user');
                     var source = $tpl.text();
                     var template = Handlebars.compile(source);
-                    var data = {user: {
-                        openid: authData.weixin.openid,
-                        nickname: authData.weixin.nickname,
-                        headUrl: authData.weixin.headimgurl
-                    }};
+                    var data = {
+                        user: {
+                            openid: authData.weixin.openid,
+                            nickname: authData.weixin.nickname,
+                            headUrl: authData.weixin.headimgurl
+                        }
+                    };
                     var html = template(data);
                     $tpl.before(html);
                     callbak(null, user[0]);
