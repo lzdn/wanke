11972850891da5e0e118fa036318e85c216cfbf6
sunshuang320 <825347993@qq.com>
2015-04-22 23:00:53 +0800

    修改bug

--- public/js/views/post_detail.js
+++ public/js/views/post_detail.js
@@ -1,14 +1,16 @@
 (function ($) {
+    var saveurl = window.location.href;
     var number = "";
     var code = "";
     var userlog, userid, queryobject, nickname, phonenumber,usersid,postId,tagvalue;
     var postview = window.location.search.split('=')[1];
-    if (postview.indexOf("=") > 0) {
-        userlog = window.location.search.split('=')[1];
-        code = userlog.split("&")[0];
-        alert(code);
-        id = ""
-    }
+    alert(postview);
+    //if (saveurl.indexOf("=") > 1) {
+    //    userlog = window.location.search.split('=')[1];
+    //    code = userlog.split("&")[0];
+    //    alert(code);
+    //    id = ""
+    //}
     // alert(postview);
     loadwx();
     loading(function () {
