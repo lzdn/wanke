7dad9d066476c14db46e7df9e6a04b1e6181d796
AmberGlasses <775837740@qq.com>
2015-04-23 17:57:39 +0800

    给易生活加入分享接口

--- routes/weixin.js
+++ routes/weixin.js
@@ -147,7 +147,7 @@ router.post('/uploadImage', function (req, res) {
             console.log(file);
             //res.json("success");
             // Execute any logic that should take place after the object is saved.
-            res.json({fileId: file.id});
+            res.json(file);
         }, function (error) {
             // The file either could not be read, or could not be saved to AV.
             res.json({error: error.message});
