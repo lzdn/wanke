14dee28224ab6ffaeaaa7354f3980c626611dd07
sunshuang320 <825347993@qq.com>
2015-04-23 17:12:09 +0800

    更新

--- public/js/views/post_index.js
+++ public/js/views/post_index.js
@@ -62,6 +62,7 @@
                                     var newtime = new Date().getTime();
                                     var imgs = object.get('relationimgs');
                                     console.log(imgs);
+                                    var query = new AV.Query
                                     var avalue = object.id;
                                     var content = object.get('content');
                                     var otagkey = object.get("tagkey");
@@ -319,44 +320,56 @@
 
     function loadwx() {
         var debug, appId, jslist, noncestr, signature, timestamp, jsApiList;
-        $.get("http://fuwuhao.dianyingren.com/weixin/getJsConfig?page=post_index", function (result) {
-            debug = result.debug;
-            appId = result.appId;
-            jslist = result.jsApiList;
-            noncestr = result.nonceStr;
-            signature = result.signature;
-            timestamp = result.timestamp;
-            jsApiList = result.jsApiList;
-            wx.config({
-                debug: debug,// 开启调试模式,调用的所有api的返回值会在客户端alert出来，若要查看传入的参数，可以在pc端打开，参数信息会通过log打出，仅在pc端时才会打印。
-                appId: appId, // 必填，公众号的唯一标识
-                timestamp: timestamp, // 必填，生成签名的时间戳
-                nonceStr: noncestr, // 必填，生成签名的随机串
-                signature: signature,// 必填，签名，见附录1
-                jsApiList: jsApiList// 必填，需要使用的JS接口列表，所有JS接口列表见附录2
-            });
-            wx.ready(function () {
-                wx.onMenuShareTimeline({
-                    title: '万科三联书社',
-                    link: window.location.href,
-                    imgUrl: 'http://fuwuhao.dianyingren.com/imgs/wankelife.jpg',
-                    success: function () {
-                    },
-                    cancel: function () {
-                    }
+        $.ajax({
+            method: "POST",
+            url: "http://fuwuhao.dianyingren.com/weixin/getJsConfig",
+            data: JSON.stringify({
+                url: window.location.href
+            }),
+            contentType: "application/json; charset=utf-8",
+            dataType: "json",
+            success: function (result) {
+                debug = result.debug;
+                appId = result.appId;
+                jslist = result.jsApiList;
+                noncestr = result.nonceStr;
+                signature = result.signature;
+                timestamp = result.timestamp;
+                jsApiList = result.jsApiList;
+                wx.config({
+                    debug: debug,// 开启调试模式,调用的所有api的返回值会在客户端alert出来，若要查看传入的参数，可以在pc端打开，参数信息会通过log打出，仅在pc端时才会打印。
+                    appId: appId, // 必填，公众号的唯一标识
+                    timestamp: timestamp, // 必填，生成签名的时间戳
+                    nonceStr: noncestr, // 必填，生成签名的随机串
+                    signature: signature,// 必填，签名，见附录1
+                    jsApiList: jsApiList// 必填，需要使用的JS接口列表，所有JS接口列表见附录2
                 });
-                wx.onMenuShareAppMessage({
-                    title: '万科三联书社',
-                    desc: '悦读 悦心 悦生活',
-                    link: window.location.href,
-                    type: 'link',
-                    imgUrl: 'http://fuwuhao.dianyingren.com/imgs/wankelife.jpg',
-                    success: function () {
-                    },
-                    cancel: function () {
-                    }
+                wx.ready(function () {
+                    wx.onMenuShareTimeline({
+                        title: '万科三联书社',
+                        link: window.location.href,
+                        imgUrl: 'http://fuwuhao.dianyingren.com/imgs/wankelife.jpg',
+                        success: function () {
+                        },
+                        cancel: function () {
+                        }
+                    });
+                    wx.onMenuShareAppMessage({
+                        title: '万科三联书社',
+                        desc: '悦读 悦心 悦生活',
+                        link: window.location.href,
+                        type: 'link',
+                        imgUrl: 'http://fuwuhao.dianyingren.com/imgs/wankelife.jpg',
+                        success: function () {
+                        },
+                        cancel: function () {
+                        }
+                    })
                 })
-            })
+            },
+            error: function (msg) {
+                alert(msg);
+            }
         });
     }
 
