5da0c225056db13289e003d20a3c841a5d264807
AmberGlasses <775837740@qq.com>
2015-04-23 17:22:12 +0800

    给易生活加入分享接口

--- public/js/views/shop_content.js
+++ public/js/views/shop_content.js
@@ -1,10 +1,11 @@
-
-(function($) {
-    var shopid=window.location.search.split('?')[1];
+(function ($) {
+    var shopid = window.location.search.split('?')[1];
     //alert(shopid);
-    loading(function(){
-        $("#users").on("click",function(){
-            window.location.href="user_detail.html";
+    loadwx(function () {
+        loading(function () {
+            $("#users").on("click", function () {
+                window.location.href = "user_detail.html";
+            });
         });
     });
 
@@ -13,41 +14,97 @@
         //ject.createWithoutData('className',id);
         var shop = AV.Object.extend("shop");
         var query = new AV.Query(shop);
-        query.equalTo("objectId",shopid);
+        query.equalTo("objectId", shopid);
         query.find({
             success: function (arry) {
                 var shops = [];
-                    var object = arry[0];
+                var object = arry[0];
                 console.log(object);
-                    shopid =object.id;
-                    shopname = object.get("shopname");
-                    shoptel = object.get("shoptel");
-                    shopaddress = object.get("shopaddress");
-                    servicetime = object.get("servicetime");
-                    shopservice = object.get("shopservice");
-                    range = object.get("range");
-                    logo=object.get("logo");
-                    var shop = {
-                        id:shopid,
-                        name: shopname,
-                        tel: shoptel,
-                        address:shopaddress,
-                        time:servicetime,
-                        service:shopservice,
-                        range:range,
-                        logo:logo
-                    };
-                    shops.push(shop);
+                shopid = object.id;
+                shopname = object.get("shopname");
+                shoptel = object.get("shoptel");
+                shopaddress = object.get("shopaddress");
+                servicetime = object.get("servicetime");
+                shopservice = object.get("shopservice");
+                range = object.get("range");
+                logo = object.get("logo");
+                var shop = {
+                    id: shopid,
+                    name: shopname,
+                    tel: shoptel,
+                    address: shopaddress,
+                    time: servicetime,
+                    service: shopservice,
+                    range: range,
+                    logo: logo
+                };
+                shops.push(shop);
                 var $tpl = $('#shopcontent');
                 var source = $tpl.text();
                 var template = Handlebars.compile(source);
                 var data = {shops: shops};
                 var html = template(data);
                 $tpl.before(html);
+
+                wx.onMenuShareTimeline({
+                    title: shopname,
+                    link: window.location.href,
+                    imgUrl: logo,
+                    success: function () {
+                    },
+                    cancel: function () {
+                    }
+                });
+                wx.onMenuShareAppMessage({
+                    title: shopname,
+                    desc: shopaddress,
+                    link: window.location.href,
+                    type: 'link',
+                    imgUrl: logo,
+                    success: function () {
+                    },
+                    cancel: function () {
+                    }
+                });
                 callbak();
             }
         });
     }
 
+    function loadwx(callback) {
+        var debug, appId, jslist, noncestr, signature, timestamp, jsApiList;
+        $.ajax({
+            method: "POST",
+            url: "http://fuwuhao.dianyingren.com/weixin/getJsConfig",
+            data: JSON.stringify({
+                url: window.location.href
+            }),
+            contentType: "application/json; charset=utf-8",
+            dataType: "json",
+            success: function (result) {
+                debug = result.debug;
+                appId = result.appId;
+                jslist = result.jsApiList;
+                noncestr = result.nonceStr;
+                signature = result.signature;
+                timestamp = result.timestamp;
+                jsApiList = result.jsApiList;
+                wx.config({
+                    debug: debug,// 开启调试模式,调用的所有api的返回值会在客户端alert出来，若要查看传入的参数，可以在pc端打开，参数信息会通过log打出，仅在pc端时才会打印。
+                    appId: appId, // 必填，公众号的唯一标识
+                    timestamp: timestamp, // 必填，生成签名的时间戳
+                    nonceStr: noncestr, // 必填，生成签名的随机串
+                    signature: signature,// 必填，签名，见附录1
+                    jsApiList: jsApiList// 必填，需要使用的JS接口列表，所有JS接口列表见附录2
+                });
+                wx.ready(function () {
 
+                    callback();
+                })
+            },
+            error: function (msg) {
+                alert(msg);
+            }
+        });
+    }
 })(jQuery);
--- public/js/views/shop_index.js
+++ public/js/views/shop_index.js
@@ -1,20 +1,21 @@
 (function ($) {
+    loadwx(function(){
+        loading(function(){
+            $(".shopcontent").on("click",function(){
+                var shopid=$(this).attr("value");
+                window.location.href="shop_content.html?"+shopid+"";
+            });
 
-    loading(function(){
-        $(".shopcontent").on("click",function(){
-            var shopid=$(this).attr("value");
-            window.location.href="shop_content.html?"+shopid+"";
-        });
-
-        $("#users").on("click",function(){
-            var currentUser = AV.User.current();
-            if (currentUser) {
-                window.location.href= "user_detail.html?"+currentUser.id+"";
-            } else {
-                $.post("http://fuwuhao.dianyingren.com/weixin/getAuthUrl",{page:"http://fuwuhao.dianyingren.com/user_detail.html"}, function (res) {
-                    window.location.href = res.authUrl;
-                })
-            }
+            $("#users").on("click",function(){
+                var currentUser = AV.User.current();
+                if (currentUser) {
+                    window.location.href= "user_detail.html?"+currentUser.id+"";
+                } else {
+                    $.post("http://fuwuhao.dianyingren.com/weixin/getAuthUrl",{page:"http://fuwuhao.dianyingren.com/user_detail.html"}, function (res) {
+                        window.location.href = res.authUrl;
+                    })
+                }
+            });
         });
     });
 
@@ -53,7 +54,61 @@
         });
     }
 
-
+    function loadwx(callback) {
+        var debug, appId, jslist, noncestr, signature, timestamp, jsApiList;
+        $.ajax({
+            method: "POST",
+            url: "http://fuwuhao.dianyingren.com/weixin/getJsConfig",
+            data: JSON.stringify({
+                url: window.location.href
+            }),
+            contentType: "application/json; charset=utf-8",
+            dataType: "json",
+            success: function (result) {
+                debug = result.debug;
+                appId = result.appId;
+                jslist = result.jsApiList;
+                noncestr = result.nonceStr;
+                signature = result.signature;
+                timestamp = result.timestamp;
+                jsApiList = result.jsApiList;
+                wx.config({
+                    debug: debug,// 开启调试模式,调用的所有api的返回值会在客户端alert出来，若要查看传入的参数，可以在pc端打开，参数信息会通过log打出，仅在pc端时才会打印。
+                    appId: appId, // 必填，公众号的唯一标识
+                    timestamp: timestamp, // 必填，生成签名的时间戳
+                    nonceStr: noncestr, // 必填，生成签名的随机串
+                    signature: signature,// 必填，签名，见附录1
+                    jsApiList: jsApiList// 必填，需要使用的JS接口列表，所有JS接口列表见附录2
+                });
+                wx.ready(function () {
+                    wx.onMenuShareTimeline({
+                        title: '万科三联书社',
+                        link: window.location.href,
+                        imgUrl: 'http://fuwuhao.dianyingren.com/imgs/wankelife.jpg',
+                        success: function () {
+                        },
+                        cancel: function () {
+                        }
+                    });
+                    wx.onMenuShareAppMessage({
+                        title: '万科三联书社',
+                        desc: '悦读 悦心 悦生活',
+                        link: window.location.href,
+                        type: 'link',
+                        imgUrl: 'http://fuwuhao.dianyingren.com/imgs/wankelife.jpg',
+                        success: function () {
+                        },
+                        cancel: function () {
+                        }
+                    });
+                    callback();
+                })
+            },
+            error: function (msg) {
+                alert(msg);
+            }
+        });
+    }
 })(jQuery);
 
 
