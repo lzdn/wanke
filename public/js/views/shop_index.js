2cd6d20871dda4fc3ae45b5c7ae9f60622911f00
sunshuang320 <825347993@qq.com>
2015-05-05 11:29:18 +0800

    样式测试

--- public/js/views/shop_index.js
+++ public/js/views/shop_index.js
@@ -1,6 +1,6 @@
 (function ($) {
     loadwx(function () {
-        $(this).css("color","#e0690c");
+        $(".shopcontent").css("color","#e0690c");
         loading(function () {
             $(".shopcontent").on("click", function () {
                 $(this).css("color","#e0690c");
