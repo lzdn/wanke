ebd0e684341480241c73188ef0184dc5ed8e5b9c
sunshuang320 <825347993@qq.com>
2015-05-05 15:12:38 +0800

    测试

--- public/js/views/shop_index.js
+++ public/js/views/shop_index.js
@@ -4,9 +4,6 @@
             $(".am-icon-phone").mousedown(function(){
                 $(this).css("color","#e0690c");
             });
-            $(".am-icon-phone").mouseup(function(){
-                $(this).css("color","red");
-            });
             $(".shopcontent").on("click", function () {
                 $(this).css("color","#e0690c");
                 var shopid = $(this).attr("value");
