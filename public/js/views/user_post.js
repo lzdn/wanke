5ce3623807b4f742be4a30a6d9ba77efdcd4bc71
sunshuang320 <825347993@qq.com>
2015-05-06 15:11:32 +0800

    测试6

--- public/js/views/user_post.js
+++ public/js/views/user_post.js
@@ -103,7 +103,6 @@
                 jsApiList: jsApiList// 必填，需要使用的JS接口列表，所有JS接口列表见附录2
             });
             wx.ready(function () {
-                alert("开始隐藏");
                 wx.hideOptionMenu();
             });
         });
