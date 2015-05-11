2b203e6d60670fc996fba42386cb278273724228
AmberGlasses <775837740@qq.com>
2015-05-11 09:52:22 +0800

    调整为开发模式

--- public/js/views/user_contact.js
+++ public/js/views/user_contact.js
@@ -60,7 +60,7 @@
 
 function load(callback) {
     var usercontact = window.location.href;
-    alert(usercontact);
+    //alert(usercontact);
     var appId, jslist, noncestr, signature, timestamp, jsApiList;
     $.post("http://fuwuhao.dianyingren.com/weixin/getJsConfig", {url: "" + usercontact + ""}, function (result) {
         appId = result.appId;
@@ -69,7 +69,7 @@ function load(callback) {
         signature = result.signature;
         timestamp = result.timestamp;
         jsApiList = result.jsApiList;
-        alert(result);
+        //alert(result);
         wx.config({
             debug: result.debug,// 开启调试模式,调用的所有api的返回值会在客户端alert出来，若要查看传入的参数，可以在pc端打开，参数信息会通过log打出，仅在pc端时才会打印。
             appId: appId, // 必填，公众号的唯一标识
