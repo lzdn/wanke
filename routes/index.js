255a43ccc7d4487a66abf0db10d422824e3e1667
AmberGlasses <775837740@qq.com>
2015-05-15 16:37:18 +0800

    关键词回复功能

--- routes/index.js
+++ routes/index.js
@@ -6,8 +6,9 @@ var AV = require('avoscloud-sdk').AV;
 AV.initialize("f7r02mj6nyjeocgqv7psbb31mxy2hdt22zp2mcyckpkz7ll8", "blq4yetdf0ygukc7fgfogp3npz33s2t2cjm8l5mns5gf9w3z");
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-    res.send('respond with a resource');
+router.get('/', function(req, res) {
+    res.send(req.query.echostr);
+    //res.send('respond with a resource');
 });
 
 module.exports = router;
