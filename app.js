a4426a0228c710a1facaa9ca49bc8508401823c1
AmberGlasses <775837740@qq.com>
2015-05-15 18:12:29 +0800

    关键词回复功能

--- app.js
+++ app.js
@@ -29,11 +29,50 @@ app.set('view engine', 'hbs');
 //app.use(favicon(__dirname + '/public/favicon.ico'));
 app.use(logger('dev'));
 app.use(bodyParser.json());
+app.use(express.query());
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', routes);
+app.use('/', wechat(config.access_token, function (req, res, next) {
+    // 微信输入信息都在req.weixin上
+    console.log(req.weixin);
+    //var message = req.weixin;
+    //if (message.FromUserName === 'diaosi') {
+    //    // 回复屌丝(普通回复)
+    //    res.reply('hehe');
+    //} else if (message.FromUserName === 'text') {
+    //    //你也可以这样回复text类型的信息
+    //    res.reply({
+    //        content: 'text object',
+    //        type: 'text'
+    //    });
+    //} else if (message.FromUserName === 'hehe') {
+    //    // 回复一段音乐
+    //    res.reply({
+    //        type: "music",
+    //        content: {
+    //            title: "来段音乐吧",
+    //            description: "一无所有",
+    //            musicUrl: "http://mp3.com/xx.mp3",
+    //            hqMusicUrl: "http://mp3.com/xx.mp3",
+    //            thumbMediaId: "thisThumbMediaId"
+    //        }
+    //    });
+    //} else {
+    //    // 回复高富帅(图文回复)
+    //    res.reply([
+    //        {
+    //            title: '你来我家接我吧',
+    //            description: '这是女神与高富帅之间的对话',
+    //            picurl: 'http://nodeapi.cloudfoundry.com/qrcode.jpg',
+    //            url: 'http://nodeapi.cloudfoundry.com/'
+    //        }
+    //    ]);
+    //}
+}));
+
+//app.use('/', routes);
 app.use('/users', users);
 app.use('/weixin', weixin);
 
