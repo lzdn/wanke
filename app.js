26e7c0b94be68be70f5a1ef1832ab168a39bd92b
AmberGlasses <775837740@qq.com>
2015-05-15 15:44:15 +0800

    关键词回复功能

--- app.js
+++ app.js
@@ -6,11 +6,6 @@ var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var hbs = require('hbs');
 var amuiHelper = require('amui-hbs-helper')(hbs);
-var wechat = require('wechat');
-var config = require('./config');
-var AV = require('avoscloud-sdk').AV;
-AV.initialize("f7r02mj6nyjeocgqv7psbb31mxy2hdt22zp2mcyckpkz7ll8", "blq4yetdf0ygukc7fgfogp3npz33s2t2cjm8l5mns5gf9w3z");
-
 
 //hbs.registerPartials(widgetDir + '/slider/src');
 hbs.registerPartial('user_accordion', "{{#this}}\n  <section data-am-widget=\"accordion\" class=\"am-accordion {{#if theme}}am-accordion-{{theme}}{{else}}am-accordion-default{{/if}}{{#if widgetId}} {{widgetId}}{{/if}}{{#if className}} {{className}}{{/if}}\"{{#if id}} id=\"{{id}}\"{{/if}} data-am-accordion='{ {{#if options.multiple}}\"multiple\": true{{/if}} }'>\n    {{#each content}}\n      <dl class=\"am-accordion-item{{#if active}} am-active{{/if}}\">\n        <dt class=\"am-accordion-title\">\n          {{{title}}}\n        </dt>\n        <dd class=\"am-accordion-bd am-collapse {{#if active}}am-in{{/if}}\">\n          <!-- 规避 Collapase 处理有 padding 的折叠内容计算计算有误问题， 加一个容器 -->\n          <div class=\"am-accordion-content am-accordion-content-extend\">\n            {{{content}}}\n          </div>\n        </dd>\n      </dl>\n    {{/each}}\n  </section>\n{{/this}}\n");
@@ -33,23 +28,9 @@ app.use(bodyParser.urlencoded({extended: false}));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use('/', routes);
 app.use('/users', users);
 app.use('/weixin', weixin);
-app.use('/', wechat(config.access_token).text(function (message, req, res, next) {
-    // TODO
-    var Keyword = AV.Object.Extend('keyword');
-    var keyword = new Keyword();
-    var query = new AV.Query(Keyword);
-    query.find({
-        success: function (results) {
-            for (var x = 0; x < results.length; x++) {
-                if (results[x].get(message) != null || results[x].get(message) != '') {
-                    res.reply(results[x].get('word'));
-                }
-            }
-        }
-    })
-}));
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
--- routes/index.js
+++ routes/index.js
@@ -1,17 +1,26 @@
 var express = require('express');
 var router = express.Router();
-var token = 'dianyingren';
+var wechat = require('wechat');
+var config = require('./config');
+var AV = require('avoscloud-sdk').AV;
+AV.initialize("f7r02mj6nyjeocgqv7psbb31mxy2hdt22zp2mcyckpkz7ll8", "blq4yetdf0ygukc7fgfogp3npz33s2t2cjm8l5mns5gf9w3z");
 
 /* GET home page. */
-router.get('/', function (req, res) {
-    var echostr = req.query.echostr;
-    res.end(echostr);
-    //res.render('index', { title: 'Express' });
-});
-
-router.get('/auth', function (req, res) {
-
-});
+router.get('/', wechat(config.access_token).text(function (message, req, res, next) {
+    // TODO
+    var Keyword = AV.Object.Extend('keyword');
+    var keyword = new Keyword();
+    var query = new AV.Query(Keyword);
+    query.find({
+        success: function (results) {
+            for (var x = 0; x < results.length; x++) {
+                if (results[x].get(message) != null || results[x].get(message) != '') {
+                    res.reply(results[x].get('word'));
+                }
+            }
+        }
+    })
+}));
 
 
 module.exports = router;
