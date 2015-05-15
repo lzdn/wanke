c426e7f425649250442515eb35eda191902a791c
AmberGlasses <775837740@qq.com>
2015-05-15 16:05:54 +0800

    关键词回复功能

--- app.js
+++ app.js
@@ -28,7 +28,7 @@ app.use(bodyParser.urlencoded({extended: false}));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', routes.reply);
+app.use('/', routes);
 app.use('/users', users);
 app.use('/weixin', weixin);
 
--- routes/index.js
+++ routes/index.js
@@ -5,19 +5,25 @@ var config = require('../config');
 var AV = require('avoscloud-sdk').AV;
 AV.initialize("f7r02mj6nyjeocgqv7psbb31mxy2hdt22zp2mcyckpkz7ll8", "blq4yetdf0ygukc7fgfogp3npz33s2t2cjm8l5mns5gf9w3z");
 
-
-exports.reply = wechat(config.access_token).text(function (message, req, res, next) {
-    // TODO
-    var Keyword = AV.Object.Extend('keyword');
-    var keyword = new Keyword();
-    var query = new AV.Query(Keyword);
-    query.find({
-        success: function (results) {
-            for (var x = 0; x < results.length; x++) {
-                if (results[x].get(message.trim()) != null || results[x].get(message.trim()) != '') {
-                    res.reply(results[x].get('word'));
-                }
-            }
-        }
-    })
+/* GET users listing. */
+router.get('/', function(req, res, next) {
+    res.send('respond with a resource');
 });
+
+module.exports = router;
+
+//exports.reply = wechat(config.access_token).text(function (message, req, res, next) {
+//    // TODO
+//    var Keyword = AV.Object.Extend('keyword');
+//    var keyword = new Keyword();
+//    var query = new AV.Query(Keyword);
+//    query.find({
+//        success: function (results) {
+//            for (var x = 0; x < results.length; x++) {
+//                if (results[x].get(message.trim()) != null || results[x].get(message.trim()) != '') {
+//                    res.reply(results[x].get('word'));
+//                }
+//            }
+//        }
+//    })
+//});
