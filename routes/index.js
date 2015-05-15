11889b6e4a5558b726f7c3cdf33a9c35049397fd
AmberGlasses <775837740@qq.com>
2015-05-15 15:44:55 +0800

    关键词回复功能

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var router = express.Router();
 var wechat = require('wechat');
-var config = require('./config');
+var config = require('../config');
 var AV = require('avoscloud-sdk').AV;
 AV.initialize("f7r02mj6nyjeocgqv7psbb31mxy2hdt22zp2mcyckpkz7ll8", "blq4yetdf0ygukc7fgfogp3npz33s2t2cjm8l5mns5gf9w3z");
 
