a4ada429fe7606fbe0564be0ccb8ddfdeca972ea
AmberGlasses <775837740@qq.com>
2015-05-15 16:00:24 +0800

    关键词回复功能

--- routes/index.js
+++ routes/index.js
@@ -5,8 +5,8 @@ var config = require('../config');
 var AV = require('avoscloud-sdk').AV;
 AV.initialize("f7r02mj6nyjeocgqv7psbb31mxy2hdt22zp2mcyckpkz7ll8", "blq4yetdf0ygukc7fgfogp3npz33s2t2cjm8l5mns5gf9w3z");
 
-/* GET home page. */
-router.get('/', wechat(config.access_token).text(function (message, req, res, next) {
+
+exports.reply = wechat(config.access_token).text(function (message, req, res, next) {
     // TODO
     var Keyword = AV.Object.Extend('keyword');
     var keyword = new Keyword();
@@ -14,13 +14,10 @@ router.get('/', wechat(config.access_token).text(function (message, req, res, ne
     query.find({
         success: function (results) {
             for (var x = 0; x < results.length; x++) {
-                if (results[x].get(message) != null || results[x].get(message) != '') {
+                if (results[x].get(message.trim()) != null || results[x].get(message.trim()) != '') {
                     res.reply(results[x].get('word'));
                 }
             }
         }
     })
-}));
-
-
-module.exports = router;
+});
