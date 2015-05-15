e0f32c5b11db55e09bc028b1f0c3ee83ceb6d000
sunshuang320 <825347993@qq.com>
2015-05-15 13:00:20 +0800

    大体完成 自定义菜单 前端样式 交互

--- public/js/views/management_menu.js
+++ public/js/views/management_menu.js
@@ -1,3 +1,30 @@
-/**
- * Created by Administrator on 2015/5/14.
- */
+(function ($) {
+    $(".menu_btn").hide();
+    $("input[name=docVlGender]:eq(0)").attr("checked",'checked');
+    $("input[name=docVlGender]").on("click",function(){
+        if($("input[name='docVlGender']:checked").val()=="null"){
+           $(".input_menu_url").attr("disabled",true);
+        }else{
+            $(".input_menu_url").val("");
+            $('.input_menu_url').attr("disabled",false);
+        }
+    })
+    $(".btn_content").mouseout(function () {
+        $(this).children().hide();
+    }).mousemove(function () {
+        $(this).children().show();
+    });
+
+    $(".add-menu-btn,.menu_btn,.menu_list,.add_menu_list").on("click",function(){
+        $('#my-prompt').modal();
+    });
+
+    $(".save_menu").on("click",function(){
+        var menuname = $(".input_menu_name").val();
+        var menuurl = $(".input_menu_url").val();
+        var menutitle=$("input[name='docVlGender']:checked").val();
+        alert(menuname+"$$$$"+menuurl+"$$$$"+menutitle);
+        $(".input_menu_name").val("");
+        $(".input_menu_url").val("");
+    });
+})(jQuery);
