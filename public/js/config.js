17d64e286a116ebf5714a76ce01193cde2634bec
sunshuang320 <825347993@qq.com>
2015-05-18 19:26:48 +0800

    测试 退出

--- public/js/config.js
+++ public/js/config.js
@@ -2,3 +2,10 @@
  * Created by Administrator on 2015/4/3.
  */
 var server = "http://wanke.dianyingren.com";
+
+function destroy_cookie(){
+    var cookie=$.AMUI.utils.cookie;
+    cookie.unset("wankeloginuseremail");
+    cookie.unset("wankeloginuserpwd");
+    window.location.href= server + '/management_login.html';
+}
