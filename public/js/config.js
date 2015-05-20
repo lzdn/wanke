a14e06a5fbc6cb561a8865fdc84d1f9f26b0f48a
AmberGlasses <775837740@qq.com>
2015-05-20 09:34:32 +0800

    修改服务器地址

--- public/js/config.js
+++ public/js/config.js
@@ -1,7 +1,7 @@
 /**
  * Created by Administrator on 2015/4/3.
  */
-var server = "http://wanke.dianyingren.com";
+var server = "http://wanke.yanshulife.com";
 
 function destroy_cookie(){
     var cookie=$.AMUI.utils.cookie;
