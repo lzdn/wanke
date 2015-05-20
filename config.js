b3472dc878d0cb045858706a7328b7b23f1e61b1
AmberGlasses <775837740@qq.com>
2015-05-20 15:22:34 +0800

    修改服务器地址

--- config.js
+++ config.js
@@ -5,7 +5,8 @@ module.exports = {
     appId: "wx83299eb9a40e040f",
     appSecret: "b4f01789803fb5afd6e06846622a3fe8",
     url: "http://wanke.dianyingren.com",
-    token: "wanke.yanshulife",
+    token: "wanke_yanshulife",
+    EncodingAESKey:"RUDUaZysSALJ1SP9YveGJn4860uCXorQhnc5mLvkd2m",
     access_token: "",
     expireTime: 0
 };
