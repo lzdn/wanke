98249c66b86f7dc762ad28ecfbe32399b66a31be
sunshuang320 <825347993@qq.com>
2015-05-20 13:57:14 +0800

    样式 统一

--- public/js/views/management_menu.js
+++ public/js/views/management_menu.js
@@ -6,15 +6,15 @@ var cookie=$.AMUI.utils.cookie;
 var useremail_cookie =cookie.get("wankeloginuseremail");
 var userpwd_cookie =cookie.get("wankeloginuserpwd");
 $(".btn_span_extent").hide();
-//if(!useremail_cookie||!userpwd_cookie){
-//    window.location.href= server + '/management_login.html?Jumpurl=management_address.html';
-//}else{
+if(!useremail_cookie||!userpwd_cookie){
+    window.location.href= server + '/management_login.html?Jumpurl=management_address.html';
+}else{
     load_menu(function (data, id) {
         cloud_id = id;
         new_data = data;
         add_event()
     });
-//}
+}
 function load_menu(callbak) {
     var query = new AV.Query(menus);
     query.find({
