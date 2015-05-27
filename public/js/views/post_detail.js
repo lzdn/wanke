2d7b8e39bc98b7fc1d66e788f158f842321a33b9
sunshuang320 <825347993@qq.com>
2015-05-27 17:20:51 +0800

    机制 用户 被冻结

--- public/js/views/post_detail.js
+++ public/js/views/post_detail.js
@@ -35,7 +35,24 @@
                             user.save()
                         }
                     });
-                    loading_event();
+                    var BlackList = AV.Object.extend('blacklist');
+                    var query = new AV.Query(BlackList);
+                    query.find({
+                        success:function(blacklist){
+                            if(blacklist.length>0){
+                                for(var i = 0 ;i<blacklist.length;i++){
+                                    if(user.id==blacklist[i].get('user_id')){
+                                        alert("您的账户已被冻结，如有疑问请联系官方");
+                                        window.location.href = "post_index.html";
+                                    }else{
+                                        loading_event();
+                                    }
+                                }
+                            }else{
+                                loading_event();
+                            }
+                        }
+                    });
                 }
             })
         });
